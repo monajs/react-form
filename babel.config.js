768329aac9853f1665695c0d82d49759dd0cc65c
yangxi <599321378@qq.com>
2020-01-07 19:06:10 +0800

    feat: 区分包babel配置和项目babel配置

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,43 @@
-module.exports = {
-  'presets': [
-    '@babel/preset-react',
-    '@babel/preset-env',
-    '@babel/preset-flow'
-  ],
-  'plugins': [
-    [
-      'import',
-      {
-        'libraryName': 'antd',
-        'libraryDirectory': 'es',
-        'style': 'css'
-      }
+console.log()
+const BABEL_ENV = process.env.BABEL_ENV
+
+let config
+
+if (BABEL_ENV === 'package') {
+  config = {
+    'presets': [
+      '@babel/preset-react',
+      '@babel/preset-env'
     ],
-    '@monajs/babel-plugin-react-css-modules',
-    'transform-react-jsx-img-import',
-    'react-hot-loader/babel',
-    'jsx-control-statements',
-    'transform-object-assign',
-    'dynamic-import-webpack',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime'
-  ]
+    'plugins': [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+} else {
+  config = {
+    'presets': [
+      '@babel/preset-react',
+      '@babel/preset-env',
+      '@babel/preset-flow'
+    ],
+    'plugins': [
+      [
+        'import',
+        {
+          'libraryName': 'antd',
+          'libraryDirectory': 'es',
+          'style': 'css'
+        }
+      ],
+      '@monajs/babel-plugin-react-css-modules',
+      'transform-react-jsx-img-import',
+      'react-hot-loader/babel',
+      'jsx-control-statements',
+      'transform-object-assign',
+      'dynamic-import-webpack',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-transform-runtime'
+    ]
+  }
 }
+module.exports = config
